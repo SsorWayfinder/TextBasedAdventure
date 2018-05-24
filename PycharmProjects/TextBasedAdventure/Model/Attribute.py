e2a310221122cca6e7e5303a3aba6300d6f67c21
Ross <rossbm94@gmail.com>
2018-05-23 21:21:12 -0400

    Adding secondary attributes to character

--- PycharmProjects/TextBasedAdventure/Model/Attribute.py
+++ PycharmProjects/TextBasedAdventure/Model/Attribute.py
@@ -1,20 +1,20 @@
 import math
 
 from Model.Levelable import Levelable
+from Model.Character import Character
 
 
 class Attribute(Levelable):
 
+    scale = 500
+    tempChange = 0
+
     def levelup(self, exp):
-        leveledUp = False
-        while exp > 0:
-            if exp > 500*self.level:
-                exp -= 500*self.level
-                self.level += 1
-                leveledUp = True
-            else:
-                exp = -1
-        return leveledUp
+        spent = 0
+        while exp > spent + self.scale*self.level:
+            spent += self.scale*self.level
+            self.level += 1
+        return spent
 
     def __init__(self, points):
         super(Attribute, self).__init__(points)
@@ -25,3 +25,61 @@ class Attribute(Levelable):
 
     def getPoints(self):
         return self.level
+
+
+class CarryWeight(Levelable):
+
+    scale = 50
+
+    def __init__(self, character: Character):
+        super(Attribute, self).__init__(0)
+        self.strength = character.str
+        return
+
+    def levelup(self, exp):
+        spent = 0
+        while exp > spent + self.scale * (self.level+self.strength):
+            spent += self.scale * self.scale * (self.level+self.strength)
+            self.level += 1
+        return spent
+
+    def getPoints(self):
+        return self.level+self.strength
+
+
+class Stamina(Levelable):
+
+    scale = 50
+
+    def __init__(self, character: Character):
+        super(Attribute, self).__init__(0)
+        self.character = character
+        self.strength = character.str
+        self.currentPoints = self.strength
+        return
+
+    def levelup(self, exp):
+        spent = 0
+        while exp > spent + self.scale * (self.level+self.strength):
+            spent += self.scale * self.scale * (self.level+self.strength)
+            self.level += 1
+        return spent
+
+    def getMaxPoints(self):
+        return self.level+self.strength
+
+    def spendStamina(self, pointsSpent):
+        self.currentPoints -= pointsSpent
+        if self.currentPoints < 0:
+            self.character.str.tempChange -= pointsSpent
+
+    def getCurrentPoints(self):
+        return self.currentPoints
+
+    def recoverStamina(self, points):
+        if self.currentPoints+points > self.getMaxPoints():
+            points = self.getMaxPoints() - self.currentPoints
+        if self.currentPoints < 0:
+            self.character.str.tempChange += points
+        self.currentPoints += points
+
--- PycharmProjects/TextBasedAdventure/Model/Character.py
+++ PycharmProjects/TextBasedAdventure/Model/Character.py
@@ -1,19 +1,26 @@
-from Model.Attribute import Attribute
+from Model.Attribute import *
 from Model.Inventory import Inventory
 
 
 class Character:
 
-    def __init__(self, name):
+    def __init__(self, name, strength, constitution, dexterity, intelligence, wisdom, charisma):
         self.name = name
-        self.str = Attribute(10)
-        self.con = Attribute(10)
-        self.dex = Attribute(10)
-        self.int = Attribute(10)
-        self.wis = Attribute(10)
-        self.cha = Attribute(10)
-        self.levelables = {
+        self.str = Attribute(strength)
+        self.con = Attribute(constitution)
+        self.dex = Attribute(dexterity)
+        self.int = Attribute(intelligence)
+        self.wis = Attribute(wisdom)
+        self.cha = Attribute(charisma)
+        self.carryWeight = CarryWeight(self)
+        self.stamina = Stamina(self)
+
+        self.levelables = [
             self.str, self.con, self.dex, self.int, self.wis, self.cha
-        }
+        ]
         self.inventory = Inventory()
+        return
+
+    def __init__(self, name):
+        self.__init__(name, 10, 10, 10, 10, 10, 10)
         return
